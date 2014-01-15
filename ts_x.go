1653efa45f46996125f5eb96a0bae453c2740b36
olekukonko <github.com@12>
2014-01-15 18:18:50 +0100

    Fixed Linux Error

--- ts_x.go
+++ ts_x.go
@@ -12,24 +12,23 @@ package ts
 import (
 	"syscall"
 	"unsafe"
+	"os"
 )
 
 // Get Windows Size
 func GetSize() (ws Size, err error) {
 
-	rc, _, ec := syscall.Syscall(syscall.SYS_IOCTL,
-		uintptr(0),
+	_ , _, ec := syscall.Syscall(syscall.SYS_IOCTL,
+		uintptr(syscall.Stdout),
 		uintptr(TIOCGWINSZ),
 		uintptr(unsafe.Pointer(&ws)))
 
-	if rc == 0 {
+	if ec != 0 {
+		err = os.NewSyscallError("SYS_IOCTL", ec)
 		// Set Default size if OS is unknown
 		if TIOCGWINSZ == 0 {
 			ws = Size{80, 25, 0, 0}
 		}
-		err = syscall.Errno(ec)
-		return ws, err
 	}
-
 	return ws, err
 }
